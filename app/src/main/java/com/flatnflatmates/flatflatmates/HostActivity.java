fb4192143de8ca2382912af72256ca19170d239d
Hitesh Goel <hitesh.goel@meritnation.com>
2015-02-19 22:29:19 +0530

    first changes

---
+++ app/src/main/java/com/flatnflatmates/flatflatmates/HostActivity.java
@@ -0,0 +1,13 @@
+package com.flatnflatmates.flatflatmates;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class HostActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host);
+    }
+}
